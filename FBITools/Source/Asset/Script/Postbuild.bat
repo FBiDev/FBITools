96c465998467c6c41c1464d89928214482514b78
FBiDev <fernando.birnfeld@gmail.com>
2024-08-05 15:30:16 -0300

    Fix: Postbuild

--- FBITools/Source/Asset/Script/Postbuild.bat
+++ FBITools/Source/Asset/Script/Postbuild.bat
@@ -1,6 +1,9 @@
 echo -------------------------------------------------------------------------------
 echo    Post-buid    ::     Move Plugins
-set SolutionName=%1
+
 robocopy "..\..\..\..\App\Assembly\data " ".\ " SQLite.Interop.dll /XO
+
+set SolutionName=%1
 robocopy ".\ " "Plugins\ " /XF *.exe *.config *.json *.manifest %SolutionName%.pdb /XD Plugins /IS /MOV
+
 if %errorlevel% leq 4 echo Post-buid ExitCode: %errorlevel%&echo:& exit 0 else exit %errorlevel%
